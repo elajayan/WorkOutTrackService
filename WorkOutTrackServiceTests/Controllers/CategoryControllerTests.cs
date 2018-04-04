4669517ac3be3949a3efcc3c5232eb4765b27498
Admin <Admin@FSD>
2018-04-03 21:16:44 -0700

    Add project files.

---
+++ WorkOutTrackServiceTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkOutTrackService.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkOutDBLayer;
+using WorkOutDBLayer.Model;
+namespace WorkOutTrackService.Controllers.Tests
+{
+    [TestClass()]
+    public class CategoryControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            CateogryDb obj = new CateogryDb();
+            int actualCount = obj.GetAll().Count;
+            int expectedCount = 4;
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            CateogryDb obj = new CateogryDb();
+            string actualCategoryName = obj.GetById(2).CategoryName;
+            string expectedCategoryName = "Walking";
+            Assert.AreEqual(expectedCategoryName, actualCategoryName);
+        }
+    }
+}
